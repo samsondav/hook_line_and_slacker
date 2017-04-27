5484522d340be6d1337a5eeddd675e52ed734143
Sam Davies <sampdavies@gmail.com>
2017-04-27 17:27:34 +0100

    We need pull request OPENED not submitted

--- lib/slack_interactor.ex
+++ lib/slack_interactor.ex
@@ -1,6 +1,20 @@
 defmodule SlackInteractor do
-  def notify_pull_request_submitted(title, url) do
-    message = "Pull request submitted - <#{url}|#{title}>"
+  require Logger
+
+  def notify_from_github_event(event) do
+    case event do
+      %{"action" => "opened", "pull_request" => pull_request} ->
+        notify_pull_request_opened(pull_request)
+      _ping = %{"zen" => zen} ->
+        Logger.debug(zen)
+        :ok
+      _ -> :ok
+    end
+  end
+
+  defp notify_pull_request_opened(pull_request) do
+    %{"url" => url, "title" => title, "user" => %{"login" => username}} = pull_request
+    message = "#{username} submitted a pull request - <#{url}|#{title}>"
     Slackex.Chat.post_message(channel(), message)
   end
 
