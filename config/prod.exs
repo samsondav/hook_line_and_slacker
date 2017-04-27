aab57855d3d705eaea10718a1b2b85b1b9012d8a
Sam Davies <sampdavies@gmail.com>
2017-04-27 16:57:01 +0100

    Add default port on production

--- config/prod.exs
+++ config/prod.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
-config :hook_line_and_slacker, port: System.get_env("PORT")
+config :hook_line_and_slacker, port: System.get_env("PORT") || 80
 
 # Do not print debug messages in production
 config :logger, level: :info
