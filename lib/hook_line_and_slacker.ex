97be0a6a40a1b1b74498cab924f992f9e03e81b1
Sam Davies <sampdavies@gmail.com>
2017-04-27 08:48:03 +0100

    Initial commit

---
+++ lib/hook_line_and_slacker.ex
@@ -0,0 +1,18 @@
+defmodule HookLineAndSlacker do
+  @moduledoc """
+  Documentation for HookLineAndSlacker.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HookLineAndSlacker.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
